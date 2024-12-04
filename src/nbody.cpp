ff0fd51bc8581da685d0d12f0564c50d585fb058
Jason Klein <jklein.64@verizon.net>
2024-12-04 03:45:34 -0500

    Add argument parsing

--- src/nbody.cpp
+++ src/nbody.cpp
@@ -1,7 +1,29 @@
 #include "nbody.h"
 #include <stdio.h>
+#include <argparse/argparse.hpp>
 
 int main(int argc, char* argv[]) {
-    printf("Hello!\n");
+    argparse::ArgumentParser program("nbody");
+    size_t w, h;
+    program.add_argument("-w", "--grid_width")
+        .scan<'i', size_t>()
+        .required()
+        .store_into(w)
+        .help("Number of horizontal grid cells.");
+    program.add_argument("-h", "--grid_height")
+        .scan<'i', size_t>()
+        .required()
+        .store_into(h)
+        .help("Number of vertical grid cells.");
+    
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    printf("Hello! Grid is %lux%lu\n", w, h);
     return 0;
 }
--- src/nbody.h
+++ src/nbody.h
@@ -1,3 +1,7 @@
 #pragma once
 
 int main(int argc, char* argv[]);
+
+void nbody_init();
+void nbody_step();
+void nbody_free();
